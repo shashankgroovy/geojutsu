2e49c24bb1f546657df478de40bfc1352cdaa4a6
Shashank Srivastav <shashankgrovy@gmail.com>
2016-06-14 15:58:31 +0530

    Added Procfile for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn geojutsu.wsgi --log-file -
--- geojutsu/settings/local.py
+++ geojutsu/settings/local.py
@@ -13,6 +13,10 @@ _MONGODB_NAME = 'mozio'
 if _MONGODB_USER:
     _MONGODB_DATABASE_HOST = 'mongodb://%s:%s@%s/%s' % (
         _MONGODB_USER, _MONGODB_PASSWD, _MONGODB_HOST, _MONGODB_NAME)
-_MONGODB_DATABASE_HOST = 'mongodb://%s/%s' % (_MONGODB_HOST, _MONGODB_NAME)
+#_MONGODB_DATABASE_HOST = 'mongodb://%s/%s' % (_MONGODB_HOST, _MONGODB_NAME)
+
+# using a mongolab instance instead.
+# TODO: Move the URI to a config variable. Don't expose the DB URI. 
+_MONGODB_DATABASE_HOST = 'mongodb://test:test@ds013564.mlab.com:13564/mozio'
 
 mongoengine.connect(_MONGODB_NAME, host=_MONGODB_DATABASE_HOST)
